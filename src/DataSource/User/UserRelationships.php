478fb967b44cfc460db19c899e00f545e697bef5
Angel S. Moreno <angelxmoreno@gmail.com>
2021-09-19 01:08:05 -0400

    Registration Service WIP

---
+++ src/DataSource/User/UserRelationships.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace TmdbProxy\DataSource\User;
+
+use Atlas\Mapper\MapperRelationships;
+
+class UserRelationships extends MapperRelationships
+{
+    protected function define()
+    {
+    }
+}
---
+++ src/Services/Auth/RegistrationService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TmdbProxy\Services\Auth;
+
+use TmdbProxy\DataSource\User\User;
+
+class RegistrationService
+{
+    public function register(array $userData): User
+    {
+        
+    }
+}
